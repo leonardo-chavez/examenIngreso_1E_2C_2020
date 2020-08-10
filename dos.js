40916b488cc9a619ddce6eae6429770606d40a5c
leonardo-chavez <68036353+leonardo-chavez@users.noreply.github.com>
2020-08-10 20:36:20 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -5,7 +5,49 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/function mostrar2(){
+  producto;
+  respuesta="si";
+  marca;
+  precio=0;
+  acumuladorPeso=0;
+  peso=0;
+  banderaLiquido=0;
+  banderaSolido=0;
+  tipo;
+
+  while(respuesta =="si"){
+      marca=prompt("Marca de este producto");
+      precio=parseFloat(pomrpt("Ingrese  precio"));
+      while (precio<0) {
+          precio=parseFloat(pomrpt("Error. Ingrese el precio nuevamente"));  
+      }
+      peso=parseFloat(prompt("Ingrese el peso en Kilos"));
+      while (peso<0) {
+          peso=parseFloat(prompt("Error. Ingrese el peso en Kilos"));
+      }
+      acumuladorPeso+=peso;
+      tipo=prompt("Que tipo de producto es solido/liquido: ").toLowerCase();
+      while (!(tipo == "solido" || tipo == "liquido")) {
+          tipo=prompt("Error. Que tipo de producto es solido/liquido: ").toLowerCase();
+      }
+      switch (tipo) {
+          case "solido":
+          if(banderaSolido==0 || banderaSolido > peso)  {
+              banderaSolido=peso;
+          } 
+              break;        
+          default:
+          if(banderaLiquido==0 || banderaLiquido<precio){
+              banderaLiquido=nombre;
+          }
+              break;
+      }
+
+
+      respuesta=prompt("Quiere ingresar otro producto? si/no");
+  }
+  console.log("a)El peso total es de " + acumuladorPeso);
+  console.log("b) el articulo más caro de los líquidos: " + banderaLiquido);
+  console.log("c)la marca del articulo más barato de los sólidos: " + banderaSolido);
 }
--- tres.js
+++ tres.js
@@ -9,7 +9,74 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+
+    let sexoTitular;
+    let banderaTitular=0;
+    let respuesta="si";
+    let lugar;
+    let temporada;
+    let cantidadPersonasViajando=0;
+    let contadorBariloche=0;
+    let contadorCataratas=0;
+    let contadorSalta=0;
+    let personasInvierno=0;
+    let promedioPersonasInvierno=0;
+    let lugarMasElegido;
+
+
+    while (respuesta == "si") {
+        sexoTitular=prompt("sexo del titular: ");
+        lugar=prompt("Ingrese el lugar: ").toLowerCase();
+        while(!(lugar == "bariloche" || lugar == "“cataratas”" || lugar == "“salta”" )){
+            lugar=prompt("Error. Ingrese el lugar: ").toLowerCase();   
+        }
+        if(lugar == "bariloche"){
+            contadorBariloche++;
+        }else if(lugar == "cataratas"){
+            contadorCataratas++;
+        }else{
+            contadorSalta++;
+        }
+        temporada=prompt("Ingrese la temporada: ").toLowerCase();
+        while (!(temporada == "otoño" || temporada == "invierno" || temporada == "verano" || temporada == "primavera")) {
+            temporada=prompt("Error. Ingrese la temporada: ").toLowerCase();    
+        }
+        cantidadPersonasViajando=parseInt(prompt("Cantidad de personas viajando : "));
+        while (cantidadPersonasViajando<1) {
+            cantidadPersonasViajando=parseInt(prompt("Error. Cantidad de personas viajando : "));
+        }
+        if(banderaTitular==0 || cantidadPersonasViajando>banderaTitular){
+            banderaTitular=sexoTitular;
+        }
+        switch (temporada) {
+            case "otoño":
+                
+                break;                
+            case "invierno":
+                promedioPersonasInvierno++  
+                personasInvierno=cantidadPersonasViajando; 
+                break;
+            case "verano":
+                
+                break;        
+            default:
+
+                break;
+        }
+              if(contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta){
+                  lugarMasElegido="Bariloche";
+              }else if(contadorCataratas >= contadorBariloche && contadorCataratas > contadorSalta){
+                  lugarMasElegido="Cataratas";
+              }else{
+                  lugarMasElegido="Salta";
+              } 
+    }
+    console.log("a)el lugar más elegido " + lugarMasElegido);
+    console.log("b)el nombre de titular que lleva más pasajeros. " + banderaTitular);
+    if(promedioPersonasInvierno>0){
+        console.log("c)el promedio de personas por viaje " + promedioPersonasInvierno +",  que viajan en invierno " + (personasInvierno/promedioPersonasInvierno));
+    }
+    
+    
 }
