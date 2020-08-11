b709f9ed55a0f284c9dbbb3db2cfc4794e539ce1
leonardo-chavez <68036353+leonardo-chavez@users.noreply.github.com>
2020-08-10 21:00:07 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -5,49 +5,98 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar2(){
-  producto;
-  respuesta="si";
-  marca;
-  precio=0;
-  acumuladorPeso=0;
-  peso=0;
-  banderaLiquido=0;
-  banderaSolido=0;
-  tipo;
+*/function mostrar()
+{
 
-  while(respuesta =="si"){
-      marca=prompt("Marca de este producto");
-      precio=parseFloat(pomrpt("Ingrese  precio"));
-      while (precio<0) {
-          precio=parseFloat(pomrpt("Error. Ingrese el precio nuevamente"));  
-      }
-      peso=parseFloat(prompt("Ingrese el peso en Kilos"));
-      while (peso<0) {
-          peso=parseFloat(prompt("Error. Ingrese el peso en Kilos"));
-      }
-      acumuladorPeso+=peso;
-      tipo=prompt("Que tipo de producto es solido/liquido: ").toLowerCase();
-      while (!(tipo == "solido" || tipo == "liquido")) {
-          tipo=prompt("Error. Que tipo de producto es solido/liquido: ").toLowerCase();
-      }
-      switch (tipo) {
-          case "solido":
-          if(banderaSolido==0 || banderaSolido > peso)  {
-              banderaSolido=peso;
-          } 
-              break;        
-          default:
-          if(banderaLiquido==0 || banderaLiquido<precio){
-              banderaLiquido=nombre;
-          }
-              break;
-      }
+	let cantProductos = 0;
+	let queProd;
+	let marca;
+	let precio = 0;
+	let precioMax = 0;
+
+	let peso;
+	let pesoMasLiviano;
+
+	let pesoTotal = 0;
+
+	let marcaMasCara;
+	let marcaMasLiviana;
+
+	let flagCaro = 0;
+	let flagLiviano = 0;
+	let respuesta = "si";
+
+	do
+	{
+	
+		do 
+		{
+
+	
+			queProd = prompt(" que producto es (solido/liquido)");
 
 
-      respuesta=prompt("Quiere ingresar otro producto? si/no");
-  }
-  console.log("a)El peso total es de " + acumuladorPeso);
-  console.log("b) el articulo más caro de los líquidos: " + banderaLiquido);
-  console.log("c)la marca del articulo más barato de los sólidos: " + banderaSolido);
+
+		} while(!(queProd == "solido" || queProd == "liquido"));
+
+		do 
+		{
+
+			precio = parseInt(prompt("Ingrese precio"));
+			marca = prompt("Ingrese marca")
+
+			if ((flagCaro == 0 || precio > precioMax) && queProd == "liquido") 
+			{
+			precioMax = precio;
+			marcaMasCara = marca;
+			flagCaro = 1;
+			}
+
+
+
+
+		} while(!(precio > 0))
+
+		do 
+		{
+
+			peso = parseFloat(prompt("Ingrese peso"));
+
+			pesoTotal = pesoTotal + peso;
+
+			if (flagLiviano == 0 || pesoMasLiviano < peso && queProd == "solido") 
+			{
+			pesoMasLiviano = peso;
+			marcaMasLiviana = marca;
+			flagLiviano = 1;
+
+
+			}	
+
+
+
+
+		} while(!(peso > 0));
+
+
+
+
+
+
+
+
+
+
+	respuesta = prompt(" queres agregar otro producto? si/no");
+
+} while(respuesta == "si");
+
+
+console.log("a)El peso total de la compra es de " + acumuladorPeso);
+console.log("b)la marca del más caro de los líquidos: " + banderaLiquido);
+console.log("c)la marca del más liviano de los sólidos: " + banderaSolido);
+
+
+
+
 }
